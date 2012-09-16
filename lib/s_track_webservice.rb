d0c52562a66d9f07768725cf29657931bac89134
Mahmoud Said <mahmoud@modsaid.com>
2012-09-16 17:45:32 +0200

    upgrading redmine to  2.0.3

--- lib/s_track_webservice.rb
+++ lib/s_track_webservice.rb
@@ -2,6 +2,6 @@
 %w{ controllers }.each do |dir| 
   path = File.join(File.dirname(__FILE__), dir)  
   $LOAD_PATH << path 
-  ActiveSupport::Dependencies.load_paths << path 
-  ActiveSupport::Dependencies.load_once_paths.delete(path) 
+  ActiveSupport::Dependencies.autoload_paths << path 
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path) 
 end 
